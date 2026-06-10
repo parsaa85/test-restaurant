9a88b49d7b3da187d4deb32c3c810f9c5a051108
parsaa85 <-->
2026-06-10 14:30:59 +0330

    fix cache

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { supabase } from "@/lib/supabase";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const { data: menu, error } = await supabase
     .from("foods")
